443473738601ff6f97ddd13c8eaa1a27975ea47b
Basti564 <bastian.schwickert@gmail.com>
2020-05-13 00:35:35 +0200

    Create package.sh

---
+++ package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "[*] Welcome to basti564's automatic fakesigner"
+for f in *.app; do
+  echo "[*] Working on ${f:0:${#f}-4}.."
+  mkdir Payload
+  echo "[*] copying files.."
+  cp -r "$f" Payload/
+  echo "[*] packaging ${f:0:${#f}-4}.."
+  zip -r "${f:0:${#f}-4}.ipa" Payload
+  echo "[*] removing payload folder.."
+  rm -f -r Payload
+done
+echo "[!] Done"
